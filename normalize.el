f2ee1251dc86c83b3be0449794e1b118b836edbf
Jesse Kelly <kellyjesse881@gmail.com>
2018-12-31 03:02:39 +0000

    Updated Package List 	modified:   normalize.el

--- normalize.el
+++ normalize.el
@@ -33,9 +33,13 @@
 
 ;;Setup Package Repositories
 (require 'package)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.milkbox.net/packages/")
-	     t)
+(setq save-package-archives '(
+       ("org"   . "http://orgmode.org/elpa/")
+       ("gnu"   . "http://elpa.gnu.org/packages/")
+       ("melpa" . "http://melpa.org/packages/")
+       ("melpa-stable" . "http://stable.melpa.org/packages/"))
+)
 (package-initialize)
+(package-refresh-contents)
 (package-install 'use-package)
 (setq use-package-always-ensure t)
