0284e942f5568a8bd3787add83eff788d64d2329
Jesse Kelly <kellyjesse881@gmail.com>
2019-09-19 16:37:47 +0000

    finished cleaning up package list

--- config/packages.el
+++ config/packages.el
@@ -1,45 +1,46 @@
 ;; Package Install And Base Config
 
-;; General purpose - Core
+;; Utility
+(use-package crux)
+(use-package hydra)
 
-;; General purpose - Non core
+;; General
+(use-package general)
 (use-package xclip :config (xclip-mode 1) :defer 1)
-(use-package beacon :init (beacon-mode 1) :config (setq beacon-color "red"))
 (use-package dumb-jump)
 (use-package docker)
 (use-package yasnippet :config (yas-global-mode 1))
 (use-package ag)
-(use-package golden-ratio :config (golden-ratio-mode 1))
 (use-package expand-region :config (setq er/try-expand-list (append er/try-expand-list '(mark-paragraph mark-page))))
-(use-package general)
 (use-package ranger :disabled)
 (use-package chronos)
 (use-package avy)
-(use-package hydra)
-(use-package general)
 (use-package projectile :config (projectile-mode))
-(use-package linum-relative)
 (use-package which-key :config (which-key-mode) (setq which-key-idle-delay 0.5))
 (use-package magit)
-(use-package crux)
-(use-package origami)
-(use-package olivetti)
 (use-package lorem-ipsum)
 (use-package company)
-(use-package coffee-mode)
 (use-package auto-complete :config (ac-config-default))
-(use-package evil :config (evil-mode 1))
-(use-package blackboard-theme :defer t)
-(use-package clues-theme :defer t)
-(use-package sublime-themes)
-(use-package monochrome-theme :defer t)
-(use-package highlight)
-(use-package fold-this)
 (use-package loccur)
 (use-package gist)
 (use-package multiple-cursors)
 
-;; Mode Specific
+;; Themes
+(use-package sublime-themes)
+(use-package blackboard-theme :defer t)
+(use-package clues-theme :defer t)
+(use-package monochrome-theme :defer t)
+(use-package golden-ratio :config (golden-ratio-mode 1))
+
+;; Aesthetics
+(use-package fold-this)
+(use-package beacon :init (beacon-mode 1) :config (setq beacon-color "red"))
+(use-package origami)
+(use-package olivetti)
+(use-package linum-relative)
+(use-package highlight)
+
+;; Modes
 (use-package nand2tetris
   :init (setq nand2tetris-core-base-dir "~/dev/projects/nand2tetris")
   :config (add-to-list 'auto-mode-alist '("\\.hdl\\'" . nand2tetris-mode))
@@ -50,6 +51,7 @@
 (use-package tide :mode ("\\.ts\\'" . web-mode))
 (use-package elpy :config (elpy-enable) :mode ("\\.py\\'" . python-mode))
 (use-package web-mode)
+(use-package coffee-mode :mode ("\\.coffee\\'" . coffee-mode))
 
 ;; Evil
 (use-package evil :config (evil-mode 1))
