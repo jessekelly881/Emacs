ac40d96035c64eea925f944136bef0d7b98c2731
Jesse Kelly <kellyjesse881@gmail.com>
2019-09-19 18:07:42 +0000

    updated modeline

--- config/aesthetics/global.el
+++ config/aesthetics/global.el
@@ -18,10 +18,19 @@
 ;; Mode Line
 (setq-default mode-line-format
   (list
-   " "
-   "%m. "
-   "%b. "
-   "line %l. "
+   '(:eval (propertize "%b " 'face 'font-lock-keyword-face
+                      'help-echo (buffer-file-name)))
+
+   (propertize "%I" 'face 'font-lock-constant-face) ;; size
+
+   " ("
+    (propertize "%l" 'face 'font-lock-type-face)
+   ")"
+   "["
+
+   '(:eval (propertize "%m" 'face 'font-lock-string-face
+                       'help-echo buffer-file-coding-system))
+   "] "
    ))
 
 ;;Highlight Curent Line
