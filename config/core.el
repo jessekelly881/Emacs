219d53f7746a83c5a3307f7f0fba1cdb08831e07
Jesse Kelly <kellyjesse881@gmail.com>
2019-09-19 13:48:13 +0000

    working on decreacing load time

--- config/core.el
+++ config/core.el
@@ -1,5 +1,5 @@
 ;; Increace gc threshold - decreaces startup by 2-3s
-(setq gc-cons-threshold 100000000)
+(setq gc-cons-threshold (* 100 1000 1000))
 
 ;; Core packages
 (load (expand-file-name "config/packages.el" user-emacs-directory))
@@ -22,6 +22,16 @@
 
 ;; Global Flycheck
 (add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; Display startup time
+(message "Emacs ready in %s with %d garbage collections."
+         (format "%.2f seconds"
+                 (float-time
+                  (time-subtract after-init-time before-init-time)))
+         gcs-done)
+
+;; Decrace gc threshold back to normal
+(setq gc-cons-threshold (* 2 1000 1000))
 
 (provide 'core)
 ;;; core.el ends here
