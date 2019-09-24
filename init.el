f7489bde93e72f82d8dc9927990e19a3263ad03e
Jesse Kelly <kellyjesse881@gmail.com>
2019-09-24 10:35:38 +0000

    commented out redundent package-initialize

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
+;; (package-initialize)
 
 (load (expand-file-name "config/core.el" user-emacs-directory))
 
