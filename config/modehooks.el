b15fb0e390de0ac6ce48e512c0b3aa202bcff975
Jesse Kelly <kellyjesse881@gmail.com>
2019-09-27 14:27:02 +0000

    allowed magit git messages to be edited in evil with org mode

--- config/modehooks.el
+++ config/modehooks.el
@@ -7,5 +7,9 @@
 (add-hook 'sgml-mode-hook 'emmet-mode)
 (add-hook 'web-mode-hook 'emmet-mode)
 
+;; Magit
+(add-to-list 'auto-mode-alist '("COMMIT_EDITMSG" . org-mode))
+(add-hook 'git-commit-mode-hook 'evil-normal-state)
+
 (provide 'modehooks)
 ;;; modehooks.el ends here
