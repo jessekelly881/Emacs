088df78890dfbbf3c98b792ff23587b6c025cdbb
Jesse Kelly <kellyjesse881@gmail.com>
2019-09-27 16:09:34 +0000

    added some more webmode hooks

--- config/modehooks.el
+++ config/modehooks.el
@@ -2,11 +2,22 @@
 (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tsx?$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.scss$" . web-mode))
 
-;; Emmet
-(add-hook 'sgml-mode-hook 'emmet-mode)
 (add-hook 'web-mode-hook 'emmet-mode)
 
+(setq web-mode-ac-sources-alist
+  '(("html" . (ac-source-emmet-html-aliases ac-source-emmet-html-snippets))
+    ("css" . (ac-source-css-property ac-source-emmet-css-snippets))))
+
+(add-hook 'web-mode-before-auto-complete-hooks
+          '(lambda ()
+             (let ((web-mode-cur-language
+                    (web-mode-language-at-pos)))
+               (if (string= web-mode-cur-language "css")
+                   (setq emmet-use-css-transform t)
+                 (setq emmet-use-css-transform nil)))))
 
 ;; Magit
 (add-hook 'git-commit-mode-hook 'evil-normal-state)
