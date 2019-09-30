dc611a552f77892d33614f3708baa6558d52eb7a
Jesse Kelly <kellyjesse881@gmail.com>
2019-09-30 13:01:05 +0000

    changed avy colors

--- init.el
+++ init.el
@@ -25,7 +25,7 @@
  '(git-gutter:modified-sign "~ ")
  '(package-selected-packages
    (quote
-    (evil-collection jedi evil-magit nord-theme fill-column-indicator org-link-minor-mode org-plus-contrib rainbow-delimiters hide-mode-line insert-shebang google-translate posframe smartparens all-the-icons beacon neotree gist dumb-jump loccur hyperbole golden-ratio go-mode go yaml-mode origami web-mode dired-sidebar helm-projectile emmet-mode docker symbol-overlay highlight highlight-symbol evil-surround evil-exchange monochrome-theme sublime-themes clues-theme blackboard-theme helm-themes crux dracula-theme git-gutter-fringe coffee-mode company tide fish-mode telephone-line mark-multiple multiple-cursors nand2tetris helm-mode-manager helm-swoop helm-chronos chronos olivetti lorem-ipsum expand-region helm-c-yasnippet helm-ag ag magit markdown-mode pug-mode avy elpy yasnippet which-key smart-mode-line linum-relative yas hydra helm use-package "use-package" ##)))
+    (nlinum-hl nlinum-relative evil-collection jedi evil-magit nord-theme fill-column-indicator org-link-minor-mode org-plus-contrib rainbow-delimiters hide-mode-line insert-shebang google-translate posframe smartparens all-the-icons beacon neotree gist dumb-jump loccur hyperbole golden-ratio go-mode go yaml-mode origami web-mode dired-sidebar helm-projectile emmet-mode docker symbol-overlay highlight highlight-symbol evil-surround evil-exchange monochrome-theme sublime-themes clues-theme blackboard-theme helm-themes crux dracula-theme git-gutter-fringe coffee-mode company tide fish-mode telephone-line mark-multiple multiple-cursors nand2tetris helm-mode-manager helm-swoop helm-chronos chronos olivetti lorem-ipsum expand-region helm-c-yasnippet helm-ag ag magit markdown-mode pug-mode avy elpy yasnippet which-key smart-mode-line linum-relative yas hydra helm use-package "use-package" ##)))
  '(send-mail-function (quote mailclient-send-it))
  '(truncate-lines t))
 (custom-set-faces
@@ -34,9 +34,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:background nil))))
+ '(avy-background-face ((t nil)))
  '(avy-goto-char-timer-face ((t (:foreground "color-52" :underline t :weight bold))))
- '(avy-lead-face ((t (:foreground "color-52" :underline t :weight bold))))
- '(avy-lead-face-0 ((t (:foreground "color-52" :underline t :weight bold))))
+ '(avy-lead-face ((t (:background "color-233" :foreground "blue"))))
+ '(avy-lead-face-0 ((t (:background "color-233" :foreground "blue"))))
  '(avy-lead-face-1 ((t (:foreground "color-52" :underline t :weight bold))))
  '(avy-lead-face-2 ((t (:foreground "color-52" :underline t :weight bold))))
  '(cursor ((t (:background "color-18" :weight bold))))
