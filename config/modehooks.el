4975b845d9a05c8f7badee540da918cdd2340ce6
Jesse Kelly <kellyjesse881@gmail.com>
2019-10-24 17:14:58 +0000

    feat(hooks): Changed web hooks

--- config/modehooks.el
+++ config/modehooks.el
@@ -1,11 +1,15 @@
 ;; Web-mode
-(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tsx?$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.css$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.scss$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html$"  . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tsx?$"  . typescript-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx?$"  . javascript-mode))
+(add-to-list 'auto-mode-alist '("\\.css$"   . css-mode))
+(add-to-list 'auto-mode-alist '("\\.scss$"  . scss-mode))
 
-(add-hook 'web-mode-hook 'emmet-mode)
+(add-hook 'web-mode-hook         'emmet-mode)
+(add-hook 'scss-mode-hook        'emmet-mode)
+(add-hook 'css-mode-hook         'emmet-mode)
+(add-hook 'javascript-mode-hook  'emmet-mode)
+(add-hook 'typescript-mode-hook  'emmet-mode)
 
 (setq web-mode-ac-sources-alist
   '(("html" . (ac-source-emmet-html-aliases ac-source-emmet-html-snippets))
