eeeece9d1cbb4136c1772d757fddff9b09185249
Jesse Kelly <kellyjesse881@gmail.com>
2020-05-14 10:42:01 -0500

    change(aesthetics): Changed normal mode cursor style

--- config/aesthetics/gui.el
+++ config/aesthetics/gui.el
@@ -2,14 +2,14 @@
 (message "Setting up GUI Config")
 
 ;; Cursor(UI Mode)
-(setq evil-normal-state-cursor '("#FFFF4D" box)
+(setq evil-normal-state-cursor '("grey" box)
           evil-insert-state-cursor '("#B30000" hbar)
           evil-visual-state-cursor '("grey" hbar)
           evil-replace-state-cursor '("#FF4D4D" hbar))
 
 ;; Font
-(set-face-attribute 'default nil :family "Iosevka" :height 80)
-(set-face-attribute 'fixed-pitch nil :family "Iosevka" :height 80)
+(set-face-attribute 'default nil :family "Fantasque Sans Mono" :height 95)
+(set-face-attribute 'fixed-pitch nil :family "Fantasque Sans Mono" :height 95)
 
 (provide 'gui)
 ;;; gui.el ends here
